1b8dbcf7e08dc919353ead0684f322e0b54ae22d
Alex Zubkov <zaleksei95@yandex.ru>
2023-09-14 15:14:12 +0300

    Функциональность «Общие фильмы».

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,7 +2,15 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
@@ -57,7 +65,7 @@ public class FilmController {
 
     @GetMapping("/films/common")
     @ResponseBody
-    public List<Film> findFilmsByLikes(@RequestParam("userId") int userId, @RequestParam("friendId") int friendId) {
+    public List<Film> findCommonFilms(@RequestParam("userId") int userId, @RequestParam("friendId") int friendId) {
         return filmService.findCommonFilms(userId, friendId);
     }
 }
